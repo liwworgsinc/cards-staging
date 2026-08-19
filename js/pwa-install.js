14adefdb6a28f4081d37c95e27a4c6a333cf0f44
liwworgsinc <liwworgsinc@gmail.com>
2026-08-18 23:21:50 -0400

    Bump Flow mobile launch cache

--- js/pwa-install.js
+++ js/pwa-install.js
@@ -15,7 +15,7 @@
     if (document.querySelector('link[data-liw-launch-fixes]')) return;
     const link = document.createElement('link');
     link.rel = 'stylesheet';
-    link.href = 'css/launch-fixes.css?v=20260818-home-mobile-1';
+    link.href = 'css/launch-fixes.css?v=20260818-flow-mobile-2';
     link.dataset.liwLaunchFixes = 'true';
     document.head.appendChild(link);
   }
