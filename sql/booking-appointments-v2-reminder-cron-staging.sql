542280cf65c2d8ff960cb399f27716d86d1c8ef7
liwworgsinc <liwworgsinc@gmail.com>
2026-09-09 15:09:13 -0400

    Use internal secret for V2 reminder cron

--- sql/booking-appointments-v2-reminder-cron-staging.sql
+++ sql/booking-appointments-v2-reminder-cron-staging.sql
@@ -17,7 +17,7 @@ select cron.schedule(
     url := 'https://nfwqcilqmqruysovjuyj.supabase.co/functions/v1/booking-reminders-v2',
     headers := jsonb_build_object(
       'Content-Type','application/json',
-      'Authorization','Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5md3FjaWxxbXFydXlzb3ZqdXlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwNzA3ODcsImV4cCI6MjEwMDY0Njc4N30.B_qZG7TP23TI1O4W_3V3vsptCT9QYontUX4kKuitvbs'
+      'x-liw-cron-secret',(select secret_value from public.booking_system_secrets where secret_key='reminder_cron_secret')
     ),
     body := jsonb_build_object('environment','staging')
   );
