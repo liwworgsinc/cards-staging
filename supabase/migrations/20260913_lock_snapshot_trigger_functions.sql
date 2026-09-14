9a67b50179a4b68078dfa83ed587e3aa28e83340
liwworgsinc <liwworgsinc@gmail.com>
2026-09-13 22:15:54 -0400

    Lock snapshot trigger functions to database triggers

---
+++ supabase/migrations/20260913_lock_snapshot_trigger_functions.sql
@@ -0,0 +1,2 @@
+revoke all on function public.liw_snapshot_card_state() from public, anon, authenticated;
+revoke all on function public.liw_snapshot_profile_state() from public, anon, authenticated;
