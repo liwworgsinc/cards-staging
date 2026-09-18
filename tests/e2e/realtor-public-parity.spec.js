ce1836efb1560e3655fedc0e3f0fe4430112f33d
liwworgsinc <liwworgsinc@gmail.com>
2026-09-18 12:45:50 -0400

    Update Realtor public regression runtime version

--- tests/e2e/realtor-public-parity.spec.js
+++ tests/e2e/realtor-public-parity.spec.js
@@ -29,7 +29,7 @@ test('shared staging loader cannot inject a second public Realtor runtime', asyn
   );
 
   expect(scripts).toHaveLength(1);
-  expect(scripts[0]).toContain('20260918-office-drawer-1');
+  expect(scripts[0]).toContain('20260918-listing-media-2');
 });
 
 
