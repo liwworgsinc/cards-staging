4b47d1c03c97a73b01cb30dd7badf768e8d52fbe
liwworgsinc <liwworgsinc@gmail.com>
2026-09-26 08:25:48 -0400

    Load per-listing showings editor and booking code on staging

--- js/public-card-liw-loader-staging.js
+++ js/public-card-liw-loader-staging.js
@@ -207,7 +207,7 @@
   }
   if(!document.querySelector('script[data-liw-booking-v1]')){
     const script=document.createElement('script');
-    script.src='js/public-booking-v1-staging.js?v=20260925-booking-selection-2';
+    script.src='js/public-booking-v1-staging.js?v=20260926-listing-hours-1';
     script.defer=true;
     script.dataset.liwBookingV1='true';
     document.body.appendChild(script);
